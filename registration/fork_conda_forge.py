209a1cd8a74352ef04acba3dbd3c5c891c763b26
Mark Harfouche <mark.harfouche@gmail.com>
2018-12-23 17:54:19 -0500

    Fix shippable some more

--- registration/fork_conda_forge.py
+++ registration/fork_conda_forge.py
@@ -74,14 +74,14 @@ def main():
     r.index.commit('Added shippable.yml')
     origin.push()
 
-                               
+
 def render_shippable(forge_dir):
     content_dir = os.path.abspath(os.path.dirname(__file__))
     env = Environment(loader=FileSystemLoader([content_dir]))
     template = env.get_template('shippable.yml.tmpl')
-                               
+
     p = Path(forge_dir) / '.ci_support'
-    config = {'configs': [(f.name, None) for f in p.glob('linux*')]}
+    config = {'configs': [(f.stem, None) for f in p.glob('linux*')]}
     shippable_config_filename = Path(forge_dir) / 'shippable.yml'
     with open(shippable_config_filename, 'w') as f:
         f.write(template.render(config))
