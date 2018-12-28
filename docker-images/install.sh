50c0a2c6e24422bedc3f391201278c13b71cd11a
Mark Harfouche <mark.harfouche@gmail.com>
2018-12-28 10:08:04 -0500

    Use the gcc tags

--- docker-images/install.sh
+++ docker-images/install.sh
@@ -1,17 +1,12 @@
 #!/bin/bash -e
 
 echo "======================= Installing gcc 7 ======================"
-echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main" >> /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main" >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 60C317803A41BA51845E371A1E9377A2BA9EF27F
-apt-get update
+#echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main" >> /etc/apt/sources.list
+#echo "deb-src http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main" >> /etc/apt/sources.list
 # bzip2 is required to install Archiconda3 (miniconda3)
 # curl jq and git are required for shippable to work without delay
-apt-get install -y gcc-7 g++-7 bzip2 curl jq git
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 80 --slave /usr/bin/g++ g++ /usr/bin/g++-7
-update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 80
-gcc --version
-g++ --version
+apt-get update
+apt-get install -y bzip2 curl jq git
 echo "================== Successfully Installed gcc 7 ==============="
 
 echo "======================== Installing Archiconda3 ==============="
